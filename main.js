d8cda56c7f906bbb275c25a87a2be57bba900598
VenkeFan <fanqi_0817@live.com>
2020-10-17 11:21:54 +0800

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 defineClass('KLLoginView', {
-  clickedLoginAction: function() {
+  clickedLoginAction: function(sender) {
    var alert = require('UIAlertController').alertControllerWithTitle_message_preferredStyle('Alert', '', 1);
    require('KLHelper').currentNavigationController().presentViewController_animated_completion(alert, true, null);
   }
